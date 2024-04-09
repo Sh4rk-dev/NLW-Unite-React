a03e8078422e34b16784be4478650a08625f600a
Renan <renanrapace13@gmail.com>
2024-04-09 19:42:03 -0300

    Creating the project table using react and tailwindCSS

---
+++ src/components/header.tsx
@@ -0,0 +1,18 @@
+import nlwUniteIcon from "../assets/nlw-unite-icon.svg";
+
+export function Header() {
+  return (
+    <div className="flex items-center py-2 gap-5">
+      <img src={nlwUniteIcon} />
+
+      <nav className="flex items-center  gap-5">
+        <a href="" className="font-medium text-sm  text-zinc-300">
+          Eventos
+        </a>
+        <a href="" className="font-medium text-sm">
+          Participantes
+        </a>
+      </nav>
+    </div>
+  );
+}
